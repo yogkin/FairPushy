5a0db1761bd6cbf82884a22ee543bdf41a7c9017
czm <yogkin@163.com>
2022-12-06 19:03:04 +0800

    init

--- server/bin/config.dart
+++ server/bin/config.dart
@@ -2,9 +2,9 @@
  * 远端数据库相关配置信息
  */
 const settingsYaml = '''
-mysql_user: 
-mysql_password: 
-mysql_host: 
-mysql_port: 
-mysql_database: 
+mysql_user: root
+mysql_password: 123456
+mysql_host: localhost
+mysql_port: 3306
+mysql_database: fair
 ''';
